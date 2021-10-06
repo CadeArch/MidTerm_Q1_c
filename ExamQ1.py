fbe2364e9244d5bf431531eb983b0b349edfe17f
Cade Rasmussen <rascade@gmail.com>
2021-10-06 15:59:42 -0600

    fixed issues finished questions and graphs

--- ExamQ1.py
+++ ExamQ1.py
@@ -4,25 +4,30 @@ import numpy as n
 class ExamQ1:
 
 
-    def __init__(self, vel, duration):
+    def __init__(self, vel, duration, startX):
         self.velocity = vel
         self.length = 4
-        self.psi = 0
-        self.xPos = 0
-        self.yPos = 0
-        self.visitedList = [[0, 0]]
+        self.theta = math.pi / 6 # 30 degrees
         self.duration = duration
+        self.psi = 0
+        self.visitedList = [[startX, 0]]
+
+        # if i wont use polar x location it can start at the beginning of unit circle
+        self.xloc = startX
+        self.yloc = 0
 
     def driveCommands(self, dt):
-        self.visitedList = [[0, 0]]
-        self.xPos = 0
-        self.yPos = 0
-        xVel = self.velocity * math.sin(math.pi / 6)
-        yVel = self.velocity * math.cos(math.pi / 6)
+
         for t in n.arange(0, self.duration, dt):
-            self.psi = self.psi + 30 * dt
-            self.xPos = self.xPos + xVel * dt
-            self.yPos = self.yPos + yVel * dt
-            self.visitedList.append([self.xPos, self.yPos])
+            # multiply in dt?
+            self.psi = self.psi + (self.velocity / self.length) * (math.tan(self.theta) * dt)
+            self.xloc = self.xloc - (self.velocity * math.sin(self.psi) * dt)
+            self.yloc = self.yloc + (self.velocity * math.cos(self.psi) * dt)
+
+            # converting to radians
+            self.radians = math.radians(self.psi)
+
+            self.visitedList.append([self.xloc, self.yloc])
 
         return self.visitedList
+
--- Main.py
+++ Main.py
@@ -1,51 +1,115 @@
+import math
+
 from ExamQ1 import ExamQ1
 from matplotlib import pyplot as p
-
-vel= 20
+import numpy as np
+length = 4
+alpha = math.pi / 6
+vel = 20
 duration = 10
-robotExam = ExamQ1(vel, duration)
+
+
+radiusPath = length / math.tan(alpha)
+print("radius: ", radiusPath)
+pathCircumf = 2 * radiusPath * math.pi
+print("path Circumfrence: ", pathCircumf)
+oneRevSec = pathCircumf / vel
+print("time for one revolution: ", oneRevSec)
+totalRev = duration / oneRevSec
+print("total revolutions", totalRev)
+oneRevDist = (vel * duration) / totalRev
+print("distance for one revolution: ", oneRevDist)
+print("total Distance traveled: ", pathCircumf * totalRev)
+print("total Distance traveled: ", oneRevDist * totalRev)
+
+endPosition = (totalRev - 4) * oneRevDist
+print("end distance into 5th revolution (arc length) ", endPosition)
+thetaEndPosition = (endPosition * 360) / (2 * math.pi * radiusPath)
+print("theta of end position", thetaEndPosition)
+radiansEndPos = math.radians(thetaEndPosition)
+gutCheck = math.radians(180) # this gives end location being (-r, 0) YAY
+bestEndX = radiusPath * math.cos(radiansEndPos)
+bestEndY = radiusPath * math.sin(radiansEndPos)
+print("theoretical end position", "( " + str(bestEndX) + ", " + str(bestEndY) + ")")
+truePoint = [bestEndX, bestEndY]
+
+robotExam = ExamQ1(vel, duration, radiusPath)
+robotExam1 = ExamQ1(vel, duration, radiusPath)
+robotExam2 = ExamQ1(vel, duration, radiusPath)
+robotExamGut = ExamQ1(vel, duration, radiusPath)
 
 def xandyPos(visitedPoints):
-    xPositions1 = []
-    yPositions1 = []
+    xlist = []
+    ylist = []
 
-    for x in visitedPoints:
-        xPositions1.append(x[0])
-        yPositions1.append(x[1])
-    return xPositions1, yPositions1
+    for point in visitedPoints:
+        xlist.append(point[0])
+        ylist.append(point[1])
+    return xlist, ylist
 
 
 visitedExam1 = robotExam.driveCommands(1)
-xPositions1sec, yPositions1sec = xandyPos(visitedExam1)
-print("\n\n (xLoc, yLoc): " , robotExam.visitedList)
-print("\n (xLoc, yLoc): ", len(robotExam.visitedList))
+xPositions1sec, yPositions = xandyPos(visitedExam1)
+print(xPositions1sec[0], yPositions[0])
+# print("\n\n (xLoc, yLoc): " , robotExam.visitedList)
+print("\ntotal points on graph: ", len(robotExam.visitedList))
+print("last point position 1 dt", "(" + str(xPositions1sec[len(xPositions1sec) - 1]) + ", " + str(yPositions[len(yPositions) - 1]) + ")" )
+print("theoretical end position", "(" + str(bestEndX) + ", " + str(bestEndY) + ")")
+dt1Point = [xPositions1sec[len(xPositions1sec) - 1], yPositions[len(yPositions) - 1]]
 
-visitedExam2 = robotExam.driveCommands(0.1)
-xPositions01, yPositions01 = xandyPos(visitedExam2)
-print("\n\n (xLoc, yLoc): " , robotExam.visitedList)
-print("\n (xLoc, yLoc): ", len(robotExam.visitedList))
+visitedExam2 = robotExam1.driveCommands(0.1)
+xPositions01, yPositions1 = xandyPos(visitedExam2)
+# print("\n\n (xLoc, yLoc): " , robotExam1.visitedList)
+print("\ntotal points on graph: ", len(robotExam1.visitedList))
+print("last point position 0.1 dt", "(" + str(xPositions01[len(xPositions01) - 1]) + ", " + str(yPositions1[len(yPositions1) - 1]) + ")" )
+print("theoretical end position", "(" + str(bestEndX) + ", " + str(bestEndY) + ")")
+dt01Point = [xPositions01[len(xPositions01) - 1], yPositions1[len(yPositions1) - 1]]
 
-visitedExam3 = robotExam.driveCommands(0.01)
-xPositions001, yPositions001 = xandyPos(visitedExam3)
-print("\n\n (xLoc, yLoc): " , robotExam.visitedList)
-print("\n (xLoc, yLoc): ", len(robotExam.visitedList))
+visitedExam3 = robotExam2.driveCommands(0.01)
+xPositions001, yPositions2 = xandyPos(visitedExam3)
+# print("\n\n (xLoc, yLoc): " , robotExam2.visitedList)
+print("\ntotal points on graph: ", len(robotExam2.visitedList))
+print("last point position 0.01 dt", "(" + str(xPositions001[len(xPositions001) - 1]) + ", " + str(yPositions2[len(yPositions2) - 1]) + ")" )
+print("theoretical end position", "(" + str(bestEndX) + ", " + str(bestEndY) + ")")
+dt001point = [xPositions001[len(xPositions001) - 1], yPositions2[len(yPositions2) - 1]]
 
+visitedExamSmall = robotExamGut.driveCommands(0.0001)
+xPositionsSmall, yPositionsSmall = xandyPos(visitedExamSmall)
+# print("\n\n (xLoc, yLoc): " , robotExamGut.visitedList)
+print("\n (xLoc, yLoc): ", len(robotExamGut.visitedList))
+print("last point position 0.01 dt", "( " + str(xPositionsSmall[len(xPositionsSmall) - 1]) + ", " + str(yPositionsSmall[len(yPositionsSmall) - 1]) + ")" )
+print("theoretical end position", "( " + str(bestEndX) + ", " + str(bestEndY) + ")")
 
-p.plot(xPositions1sec, yPositions1sec, marker='o', color="green")
-p.title("error - diff d/t 1, 0.1, 0.01")
-p.ylabel("yPos (meters)")
-p.xlabel("xPos (meters)")
-p.show()
+#
+# p.plot(xPositions1sec, yPositions, lw="2.5")
+# p.title("dt 1")
+# p.show()
+#
+# p.plot(xPositions01, yPositions1, marker='o', color="orange")
+# p.title("dt 0.1")
+# p.show()
+#
+# p.plot(xPositions001, yPositions2, marker='o', color="black")
+# p.title("dt 0.01")
+# p.show()
 
-p.plot(xPositions01, yPositions01, marker='o', color="orange")
-p.title("error - diff d/t 1, 0.1, 0.01")
-p.ylabel("yPos (meters)")
-p.xlabel("xPos (meters)")
-p.show()
+# distance formula
+def distance(point1, point2):
+    dis = math.sqrt((point2[0] - point1[0])**2 + (point2[1] - point1[1])**2)
+    return dis
 
-p.plot(xPositions001, yPositions001, marker='o', color="black")
-p.title("error - diff d/t 1, 0.1, 0.01")
-p.ylabel("yPos (meters)")
-p.xlabel("xPos (meters)")
+# distance between final point of each different robot with different time steps
+error = distance(truePoint,dt1Point)
+error1 = distance(truePoint,dt01Point)
+error2 = distance(truePoint,dt001point)
+
+# plotting errors
+fig = p.figure()
+langs = ['dt-1', 'dt-0.1', 'dt-0.01']
+metersOff = [error, error1, error2]
+p.bar(langs,metersOff)
+p.xlabel("time steps in seconds")
+p.ylabel("error in meters")
+p.title("error in different time steps")
 p.show()
 
